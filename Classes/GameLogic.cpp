68f08fd0e50375c31c2c68605b1070f694e893e8
junggang <missjingi@gmail.com>
2014-01-18 06:32:00 -0800

    gameData 75%

--- Classes/GameLogic.cpp
+++ Classes/GameLogic.cpp
@@ -38,7 +38,7 @@ bool CGameLogic::init()
 		case 0:
 			m_Character[0].m_CharacterName = "Green";
 			m_Character[0].m_CharacterColor = ccc4(_COLOR_CHARACTER_1_) ;
-			m_Character[0].m_CharacterSelectImage = "CHARACTER_1_Play.png";
+			m_Character[0].m_CharacterSettingImage = "CHARACTER_1_Play.png";
 			m_Character[0].m_CharacterPlayImage = "CHARACTER_1_Play.png";
 			m_Character[0].m_CharacterResultImage = "PLAYER_face1.png";
 			m_Character[0].m_CharacterVoice = "Sound/PLAYER_1_good.mp3";
@@ -46,7 +46,7 @@ bool CGameLogic::init()
 		case 1:
 			m_Character[1].m_CharacterName = "Blue";
 			m_Character[1].m_CharacterColor = ccc4(_COLOR_CHARACTER_2_ );
-			m_Character[1].m_CharacterSelectImage = "CHARACTER_2_Play.png";
+			m_Character[1].m_CharacterSettingImage = "CHARACTER_2_Play.png";
 			m_Character[1].m_CharacterPlayImage = "CHARACTER_2_Play.png";
 			m_Character[1].m_CharacterResultImage = "PLAYER_face2.png";
 			m_Character[1].m_CharacterVoice = "Sound/PLAYER_2_good.mp3";			
@@ -54,7 +54,7 @@ bool CGameLogic::init()
 		case 2:
 			m_Character[2].m_CharacterName = "Red";
 			m_Character[2].m_CharacterColor = ccc4(_COLOR_CHARACTER_3_ );
-			m_Character[2].m_CharacterSelectImage = "CHARACTER_3_Play.png";
+			m_Character[2].m_CharacterSettingImage = "CHARACTER_3_Play.png";
 			m_Character[2].m_CharacterPlayImage = "CHARACTER_3_Play.png";
 			m_Character[2].m_CharacterResultImage = "PLAYER_face3.png";
 			m_Character[2].m_CharacterVoice = "Sound/PLAYER_3_good.mp3";
@@ -62,7 +62,7 @@ bool CGameLogic::init()
 		case 3:
 			m_Character[3].m_CharacterName = "Yellow";
 			m_Character[3].m_CharacterColor =ccc4( _COLOR_CHARACTER_4_) ;
-			m_Character[3].m_CharacterSelectImage = "CHARACTER_4_Play.png";
+			m_Character[3].m_CharacterSettingImage = "CHARACTER_4_Play.png";
 			m_Character[3].m_CharacterPlayImage = "CHARACTER_4_Play.png";
 			m_Character[3].m_CharacterResultImage = "PLAYER_face4.png";
 			m_Character[3].m_CharacterVoice = "Sound/PLAYER_4_good.mp3";	
@@ -72,4 +72,45 @@ bool CGameLogic::init()
 	}
 
 	return true;
+}
+
+void CGameLogic::SetMapSize( int x, int y )
+{
+	m_MapSize.m_Height = y;
+	m_MapSize.m_Width = x;
+}
+
+void CGameLogic::UpdatePlayerResult( int playerId, MO_ITEM item )
+{
+	++m_PlayerData[playerId].m_MyTile;
+
+	switch (item)
+	{
+	case MO_GOLD:
+		++m_PlayerData[playerId].m_MyGold;
+		break;
+	case MO_TRASH:
+		++m_PlayerData[playerId].m_MyTrash;
+		break;
+	default:
+		break;
+	}
+}
+
+int CGameLogic::GetPlayerResult(int playerId, MO_ITEM item)
+{
+	switch (item)
+	{
+	case MO_NOTHING:
+		return m_PlayerData[playerId].m_MyTile;
+		break;
+	case MO_GOLD:
+		return m_PlayerData[playerId].m_MyGold;
+		break;
+	case MO_TRASH:
+		return m_PlayerData[playerId].m_MyTrash;
+		break;
+	default:
+		break;
+	}
 }
--- Classes/GameLogic.h
+++ Classes/GameLogic.h
@@ -36,7 +36,7 @@ struct Character
 
 	cocos2d::ccColor4B m_CharacterColor;
 
-	std::string m_CharacterSelectImage;
+	std::string m_CharacterSettingImage;
 	std::string m_CharacterPlayImage;
 	std::string m_CharacterResultImage;
 	std::string m_CharacterVoice;
@@ -54,11 +54,38 @@ public:
 
 	bool init();
 
+	int GetPlayerId(int playerIdx) {return m_PlayerData[playerIdx].m_PlayerId; }
+
+	//캐릭터를 플레이어에게 짝지어 준다.
+	void SetPlayerCharacterId(int playerId, int characterId) { m_PlayerData[playerId].m_CharacterId = characterId;}
+
+	const std::string& GetPlayerName(int playerId)			{ return m_PlayerData[playerId].m_PlayerName; }
+	
+	const std::string& GetPlayerSettingImage(int playerId)		{ return m_Character[m_PlayerData[playerId].m_CharacterId].m_CharacterSettingImage; }
+	const std::string& GetPlayerPlayImage(int playerId)			{ return m_Character[m_PlayerData[playerId].m_CharacterId].m_CharacterPlayImage; } 
+	const std::string& GetPlayerResultImage(int playerId) { return m_Character[m_PlayerData[playerId].m_CharacterId].m_CharacterResultImage; }
+	const cocos2d::ccColor4B& GetPlayerColor(int playerId) { return m_Character[m_PlayerData[playerId].m_CharacterId].m_CharacterColor; }
+
+	void SetPlayerTurn(int playerId, int turn)				{ m_PlayerData[playerId].m_PlayerTurn = turn; }
+	int GetPlayerTurn(int palyerId)							{ return m_PlayerData[palyerId].m_PlayerTurn; }
+
+	void		SetMapSize(int x, int y);
+	MapSize		GetMapSize() {return m_MapSize;}
+
+	void		SetPlayerNumber(int playerNumber)			{m_PlayerNumber = playerNumber;}
+	int			GetplayerNumber()							{return m_PlayerNumber; }
+
+	void		UpdatePlayerResult(int playerId, MO_ITEM item);
+	int			GetPlayerResult(int playerId, MO_ITEM item);
+
+	void		UpdatePlayerScore(int playerId, int score) { m_PlayerData[playerId].m_MyTotalScore += score; }
+	int			GetPlayerTotalScore(int playerId) {return m_PlayerData[playerId].m_MyTotalScore; }
+
 private:
 	static CGameLogic*	m_pInstance; //singleton instance
 
-
 	MapSize				m_MapSize;
+	int						m_PlayerNumber;
 
 	std::array<PlayerData, MAX_PLAYER_NUM> m_PlayerData;
 	std::array<Character, CHARACTER_NUM> m_Character;
